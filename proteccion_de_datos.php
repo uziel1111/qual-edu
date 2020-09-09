202bfb1246e905934f764123836a7bc8e10ed99f
uziel1111 <uziel.123@gmail.com>
2020-09-09 09:22:55 -0500

    ajustes

---
+++ proteccion_de_datos.php
@@ -0,0 +1,38 @@
+<?php include 'theme/template/header.php'; ?>
+        <!--================ Inner section header Area =================-->
+        <section class="pricing_area purple_price" id="price">
+            <div class="container">
+                <div class="main_white_title">
+                    <h2>Protección de datos</h2>
+                </div>
+            </div>
+        </section>
+    <!--================End Inner section header Area =================-->
+    <!--================ Contact Area =================-->
+
+        <section class="works_area">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col">
+                        <div class="works_right_content">
+                            <div class="content_title purple_content">
+                                <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+                            </div>
+							<p>Lorem ipsum dolor sit amet consectetur adipiscing elit eleifend, bibendum venenatis est cursus semper euismod conubia suscipit sem, etiam fringilla varius enim diam quisque ante. Leo id malesuada velit congue integer parturient conubia, auctor commodo nullam torquent dictumst sem. Nam ullamcorper rutrum nec sapien quam iaculis maecenas taciti, mus dignissim orci suspendisse inceptos eu.
+
+Habitant at himenaeos aptent mi leo morbi venenatis rhoncus ridiculus hac vehicula, primis curae duis fames ornare luctus ac platea libero arcu quisque, curabitur lobortis varius praesent eget mauris eu porttitor dignissim sollicitudin. Tempus ac urna habitasse montes sollicitudin euismod netus cursus litora vivamus parturient elementum, venenatis ornare consequat nec imperdiet viverra conubia nisl mi sociis augue. Sodales fermentum ante leo gravida posuere morbi, curae tortor erat in fusce quis, viverra phasellus fringilla netus tincidunt.</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                <div class="right_question_img">
+                    <!-- <img class="img-fluid" src="theme/img/contactt-1.png" alt=""> -->
+                </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <!--================ Contact Area =================-->
+
+<?php include 'theme/template/footer.php'; ?>
--- theme/template/header.php
+++ theme/template/header.php
@@ -15,23 +15,23 @@ $url = 'http://'.$_SERVER['SERVER_NAME'].'/qual-edu/';
         <link href="theme/vendors/themify-icon/themify-icons.css" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
-        
+
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        
+
         <!-- RS5.0 Layers and Navigation Styles -->
         <link rel="stylesheet" type="text/css" href="<?php echo $url;?>theme/vendors/revolution/css/layers.css">
         <link rel="stylesheet" type="text/css" href="<?php echo $url;?>theme/vendors/revolution/css/navigation.css">
         <link rel="stylesheet" type="text/css" href="<?php echo $url;?>theme/vendors/revolution/css/settings.css">
-        
+
         <!-- Extra plugin css -->
         <link href="<?php echo $url;?>theme/vendors/animate-css/animate.css" rel="stylesheet">
         <link href="<?php echo $url;?>theme/vendors/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
         <link href="<?php echo $url;?>theme/vendors/flipster/jquery.flipster.css" rel="stylesheet">
-        
+
         <link href="<?php echo $url;?>theme/css/style.css" rel="stylesheet">
         <link href="<?php echo $url;?>theme/css/responsive.css" rel="stylesheet">
-        
+
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
@@ -40,11 +40,11 @@ $url = 'http://'.$_SERVER['SERVER_NAME'].'/qual-edu/';
         <![endif]-->
     </head>
     <body>
-       
+
         <!--================Slider Area =================-->
         <header class="main_menu_area">
 <nav class="navbar navbar-expand-lg">
-<div class="container">	
+<div class="container">
     <div class="d-flex flex-grow-1">
         <span class="w-100 d-lg-none d-block"><!-- hidden spacer to center brand on mobile --></span>
         <a class="navbar-brand d-none d-lg-inline-block my-2" href="#">
@@ -66,6 +66,7 @@ $url = 'http://'.$_SERVER['SERVER_NAME'].'/qual-edu/';
                             <li class="nav-item"><a href="./equipo.php">Equipo</a></li>
                             <li class="nav-item"><a href="./clientes.php">Clientes</a></li>
                             <li class="nav-item"><a href="./contacto.php">Contacto</a></li>
+                            <li class="nav-item"><a href="./proteccion_de_datos.php">protección de datos</a></li>
 <!--                            <li class="nav-item dropdown submenu">
                                 <a href="#blog" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
                                 <i class="fa fa-chevron-down click_btn" aria-hidden="true"></i>
@@ -80,7 +81,7 @@ $url = 'http://'.$_SERVER['SERVER_NAME'].'/qual-edu/';
                         </ul>
   </div>
 </div>
-	
+
 </nav>
         </header>
         <!--================Header Area End =================-->
